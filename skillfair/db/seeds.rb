de8a95de10a064c5efad6f0a7c75414808de6936
EC2 Default User <ec2-user@ip-172-31-91-49.ec2.internal>
2025-05-22 21:30:58 +0000

    Final exam complete

---
+++ skillfair/db/seeds.rb
@@ -0,0 +1,7 @@
+# db/seeds.rb
+Product.create([
+  { label: "kitchen", image_url: "https://content.codecademy.com/projects/make-a-website/lesson-4/kitchen.jpg" },
+  { label: "woodwork", image_url: "https://content.codecademy.com/projects/make-a-website/lesson-4/woodwork.jpg" },
+  { label: "gifts", image_url: "https://content.codecademy.com/projects/make-a-website/lesson-4/gifts.jpg" },
+  { label: "antiques", image_url: "https://content.codecademy.com/projects/make-a-website/lesson-4/antique.jpg" }
+])
